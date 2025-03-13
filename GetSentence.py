25443fe01cc8be3b6832d0736cadc3f6ff1eddad
SOLID <solidbiker3000@gmail.com>
2025-03-13 05:35:22 -0500

    Small bug fix

--- GetSentence.py
+++ GetSentence.py
@@ -1,7 +1,7 @@
 SentenceNumber = input("Enter the sentence number: ")
 SentenceNumber = int(SentenceNumber)
 
-with open("Sentences") as fp:
+with open("Sentences.txt", encoding="utf-8") as fp:
     for i, line in enumerate(fp):
         if i == SentenceNumber:
             print(line)
